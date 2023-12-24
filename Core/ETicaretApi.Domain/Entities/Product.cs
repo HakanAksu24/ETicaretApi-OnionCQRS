0163c0c14d88950c21425a0a19a97d1720913e0e
HakanAksu24 <132559884+HakanAksu24@users.noreply.github.com>
2023-12-25 02:17:17 +0300

    DbContext oluşturuldu.

--- Core/ETicaretApi.Domain/Entities/Product.cs
+++ Core/ETicaretApi.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaretApi.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ETicaretApi.Domain.Entities
 {
-    public class Product
+    public class Product : EntityBase
     {
         public required string Title { get; set; }
         public required string Description { get; set; }
---
+++ Infrastructure/ETicaretApi.Persistance/Registration.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretApi.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Persistance
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
